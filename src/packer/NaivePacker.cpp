103dc8f64926bbce23a2f0311d1fed79ba3922b2
Dawid Tracz <dawid.b.tracz@gmail.com>
2023-09-02 23:38:27 +0200

    simplest packer working

--- src/packer/NaivePacker.cpp
+++ src/packer/NaivePacker.cpp
@@ -1,6 +1,11 @@
 #include "packer/NaivePacker.h"
 #include "base/parts.hpp"
+#include "core/IndentWriter.h"
 #include "globals.h"
+#include "parser/Preparser.h"
+#include "parser/scad_2D_parser.hpp"
+#include <cmath>
+#include <sstream>
 #include <vector>
 
 
@@ -34,20 +39,24 @@ fill_sheet(Sheet* sheet, const std::vector<FlatPart>& parts)
     for (auto part : parts) {
         auto hull = rectangular_hull(part, SHEET_IN_BETWEEN_MARGIN / 2);
         double width = hull[0].x - hull[2].x;
-        double height = hull[2].y - hull[0].y;
-        if (height > sheet->dims.y || width > sheet->dims.x)
-            throw std::runtime_error(
-                "One of components is to big to be fitted in given sheet"
-            );
+        double height = hull[0].y - hull[2].y;
+        if (height > sheet->dims.y || width > sheet->dims.x) {
+            std::stringstream msg;
+            msg << "One of components is to big (" << width << ", " << height
+                << ") to be fitted in given sheet (" << sheet->dims.x << ", "
+                << sheet->dims.y << ")";
+            throw std::runtime_error(msg.str());
+        }
         if (position.x + width > sheet->dims.x) {
-            position = { 0, next_row_position };
+            position = { 0, position.y + next_row_position };
             next_row_position = 0;
         }
         if (position.y + height > sheet->dims.y) {
             leftovers.push_back(part);
             continue;
         }
-        Vec2 move_by = hull[2] - position;
+        Vec2 move_by = position - hull[2];
+        next_row_position = std::max(next_row_position, height);
         part.translate({ move_by.x, move_by.y, 0 });
         sheet->add(part);
         position.x += width;
@@ -75,7 +84,20 @@ std::vector<Sheet> NaivePacker::pack()
 }
 
 
-std::vector<Vec2> rectangular_hull(const FlatPart& part, double margin)
+std::vector<Vec2> packer::rectangular_hull(const FlatPart& part, double margin)
 {
-    // TODO
+    IndentWriter writer;
+    part.render2D(writer);
+    parser::Preparser preparser;
+    preparser << writer;
+    auto parser_ptr = std::make_shared<parser::Scad2DParser>();
+    preparser >> *parser_ptr;
+    std::shared_ptr<parser::Shape> shape
+        = std::dynamic_pointer_cast<parser::Shape>(parser_ptr->get_parsed());
+    std::vector<Vec2> hull = shape->get_hull();
+    hull[0] += { margin, margin };
+    hull[1] += { -margin, margin };
+    hull[2] += { -margin, -margin };
+    hull[3] += { margin, -margin };
+    return hull;
 }
